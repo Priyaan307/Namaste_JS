c90fbb578ada287a508bad1e2c0a49b3017055e7
priyadarshani <priyarrsdce@gmail.com>
2024-02-19 12:22:26 +0530

    JS

---
+++ day68.js
@@ -0,0 +1,37 @@
+// case: 1
+
+// var x = 8;
+
+// function getName() {
+//     console.log("Priyadarshani!!");
+// }
+// getName();
+// console.log(x);
+
+// Output: 
+// Priyadarshani!!
+// 8
+
+// Case: 2
+
+// getName();
+// console.log(x);
+
+// var x = 8;
+
+// function getName() {
+//     console.log("Priyadarshani!!");
+// }
+
+
+// Output: 
+// Priyadarshani!!
+// undefined
+
+console.log(x);
+var x = 8;
+console.log(getName);
+function getName() {
+    console.log("Priyadarshani!!");
+}
+
